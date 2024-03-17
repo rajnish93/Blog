b8dac66644fddb360e9ed6f59fa4c181a65c133c
Rajnish Singh <10815566+rajnish93@users.noreply.github.com>
2024-03-17 23:49:35 +0530

    added tag dynamic route

---
+++ app/tags/[tag]/page.tsx
@@ -0,0 +1,20 @@
+import BlogList from "@/components/blogList";
+import { getAllFilesFrontMatter, getAllTags } from "@/lib/mdx";
+import kebabCase from "@/lib/utils/kebabCase";
+
+interface pageProps {
+  params: { tag: string };
+}
+
+export default function TagPage({ params }: pageProps) {
+  const allPosts = getAllFilesFrontMatter("blog");
+  const filteredPosts = allPosts.filter(
+    (post) =>
+      !post.draft && post.tags.map((t) => kebabCase(t)).includes(params.tag)
+  );
+
+  const title =
+    params.tag[0].toUpperCase() + params.tag.split(" ").join("-").slice(1);
+
+  return <BlogList posts={filteredPosts} title={title} />;
+}
